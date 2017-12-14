05ab61b0bf9d176e3610c1e72c6939a9d5c4e45f
talktoyaminipriya <talktoyaminipriya@gmail.com>
2017-12-15 01:36:55 +0530

    Update Smsreceiver.java

--- app/src/main/java/com/example/priya/smsreceiver/Smsreceiver.java
+++ app/src/main/java/com/example/priya/smsreceiver/Smsreceiver.java
@@ -1,5 +1,6 @@
 package com.example.priya.smsreceiver;
 
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,6 +12,8 @@ import android.widget.Toast;
  * Created by Priya on 12/14/2017.
  */
 
+// sms receiver class
+
 public class Smsreceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -18,12 +21,19 @@ public class Smsreceiver extends BroadcastReceiver {
         Object[] puds = (Object[]) pudsbundle.get("puds");
         SmsMessage messages = SmsMessage.createFromPdu((byte[])puds[0]);
 
+        
+        // start application main activity
 
         Intent smsIntent = new Intent (context, MainActivity.class);
+        
+        // send message and number in action
+        
         smsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         smsIntent.putExtra("MessageNumber", messages.getOriginatingAddress());
         smsIntent.putExtra("Message",messages.getMessageBody());
         context.startActivity(smsIntent);
+        
+         // show message in toast
         Toast.makeText(context,"SMS Received from :"+ messages.getOriginatingAddress()
                         +"\n"+messages.getMessageBody()
         ,Toast.LENGTH_LONG).show();
